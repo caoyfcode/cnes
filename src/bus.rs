a3e6e35a29f154cab1c50790a89805436aa47921
caoyf <yf1711240971@mail.ustc.edu.cn>
2022-07-28 00:57:57 +0800

    添加CPU地址空间注释

--- src/bus.rs
+++ src/bus.rs
@@ -1,5 +1,37 @@
 use crate::cpu::Mem;
 
+//  _______________ $10000  _______________
+// | PRG-ROM       |       |               |
+// | Upper Bank    |       |               |
+// |_ _ _ _ _ _ _ _| $C000 | PRG-ROM       |
+// | PRG-ROM       |       |               |
+// | Lower Bank    |       |               |
+// |_______________| $8000 |_______________|
+// | SRAM          |       | SRAM          |
+// |_______________| $6000 |_______________|
+// | Expansion ROM |       | Expansion ROM |
+// |_______________| $4020 |_______________|
+// | I/O Registers |       |               |
+// |_ _ _ _ _ _ _ _| $4000 |               |
+// | Mirrors       |       | I/O Registers |
+// | $2000-$2007   |       |               |
+// |_ _ _ _ _ _ _ _| $2008 |               |
+// | I/O Registers |       |               |
+// |_______________| $2000 |_______________|
+// | Mirrors       |       |               |
+// | $0000-$07FF   |       |               |
+// |_ _ _ _ _ _ _ _| $0800 |               |
+// | RAM           |       | RAM           |
+// |_ _ _ _ _ _ _ _| $0200 |               |
+// | Stack         |       |               |
+// |_ _ _ _ _ _ _ _| $0100 |               |
+// | Zero Page     |       |               |
+// |_______________| $0000 |_______________|
+const RAM: u16 = 0x0000; // RAM 起始地址
+const RAM_MIRRORS_END: u16 = 0x1fff; // RAM 映射截止
+const PPU_REGISTERS: u16 = 0x2000; // PPU Registers 起始地址
+const PPU_REGISTERS_MIRRORS_END: u16 = 0x3fff; // PPU Registers 映射截止
+
 pub struct Bus {
     cpu_vram: [u8; 2048],  // 2KB CPU VRAM
 }
@@ -11,11 +43,6 @@ impl Bus {
         }
     }
 }
-
-const RAM: u16 = 0x0000; // RAM 起始地址
-const RAM_MIRRORS_END: u16 = 0x1fff; // RAM 映射截止
-const PPU_REGISTERS: u16 = 0x2000; // PPU Registers 起始地址
-const PPU_REGISTERS_MIRRORS_END: u16 = 0x3fff; // PPU Registers 映射截止
 
 impl Mem for Bus {
     fn mem_read(&self, addr: u16) -> u8 {
